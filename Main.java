1026b5e4d6c3e2a10f0ef74908b4bea76401b4a1
luismdsleite <luismdsleite@gmail.com>
2021-04-06 20:54:07 +0100

    final version

--- Main.java
+++ Main.java
@@ -1,37 +1,21 @@
 import java.util.*;
-
+ 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        PoligSolver p1 = new PoligSolver(40,20);
-        System.out.println(p1.details());
-        MyPoint[] save = p1.state.clone();
-        
-        p1.simulatedAnnealing(100000, 0.995);
-        System.out.println(p1.interCount());
-        
-        p1.state = save;
-        p1.ACO(100, 20, 1, 5, 500, 0.5, true);
-        System.out.println(p1.interCount());
+        PoligSolver Exemplo1 = new PoligSolver(in);
+        PoligSolver Exemplo2 = new PoligSolver(in);
 
+        Exemplo1.ACO(10000, 5, 1, 5, 500, 0.5, true);
+        Exemplo2.simulatedAnnealing(100000, 0.995);
+
+        System.out.println(Exemplo1.interCount() + " " + Exemplo1.perimeterCount());
+        
+        ArrayList<Integer> intersectionsPoints = new ArrayList<>();
+        System.out.println(Exemplo2.interCount(intersectionsPoints) + " "+ Exemplo2.perimeterCount());
+
+        int xres=1366, yres=768, scale = 10;
+        Exemplo1.showGraph(xres, yres, scale);
+        Exemplo2.showGraph(xres, yres, scale);
     }
 }
-
-/*
-PARA RESULTADOS
-Para ajudar a mostrar que quando maxIter aumenta a função converge
-public static void main(String[] args) {
-        double avg = 0;
-        for(int i=0;i<40;i++){
-            PoligSolver p1 = new PoligSolver(40,20);
-            MyPoint[] save = p1.state;
-            p1.ACO2(100, 10, 1, 5, 500, 0.5, true);
-            int perimeter1 = p1.perimeterCount();
-            p1.state = save;
-            p1.ACO2(10, 10, 1, 5, 500, 0.5, true);
-            int perimeter2 = p1.perimeterCount();
-            avg += (perimeter1 - perimeter2) / ((double)perimeter1+perimeter2);
-        }
-        System.out.println((avg/20)*100);
-    } 
-*/
